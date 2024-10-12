105ec8fc2db241386b6cb11ce85bb97ba7326ade
Beksultan Chomoev <chomoevbeksultan@gmail.com>
2024-10-12 16:01:05 -0500

    Added handleChange function to input field in Search component.

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,9 +1,18 @@
+import React from "react";
 
-const Search = () => (
+const Search = () => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e);
+
+        console.log(e.target.value);
+    }
+
+    return (
         <div>
-            <label htmlFor="search">Search</label>
-            <input id='search' type="text"/>
+            <label htmlFor="search">Search: </label>
+            <input id='search' type="text" onChange={handleChange} />
         </div>
-)
+    );
+};
 
 export default Search;
